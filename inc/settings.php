44167c06dcb863889980cc81a9c46139ce95445c
artuurslv <artuurslv@gmail.com>
2014-12-11 20:03:31 +0000

    connection settup

--- inc/settings.php
+++ inc/settings.php
@@ -1,5 +1,5 @@
 <?php
-define ("PATH_SITE",        "http://localhost/qconstr/");
+define ("PATH_SITE",        "http://localhost/flashCMS/");
 define ("PATH_ADMIN",        PATH_SITE."admin/");
 define ("PATH_UPLOAD",        "../upload"); //Images path for tinymince
 define ("GALERY_UPLOAD", 	  "../upload/galery/");
